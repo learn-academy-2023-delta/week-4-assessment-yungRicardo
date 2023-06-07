1b79ec5e65bf82ef00fa1b454cf36afa4aaa8ba6
yungRicardo <rickyjespinoza@gmail.com>
2023-06-07 06:39:17 -0700

    week 4 assessment

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -27,9 +27,29 @@ const colors2 = [
 ]
 // Expected output example (can be a different order): ["saffron", "aquamarine", "periwinkle", "indigo", "ochre"]
 
+
+// describe("shuffleColors", () => {
+//   it("return array with frist item removed and the rest shuffled", () => {
+//     expect(shuffleColors(colors1)).toEqual(shuffleColors(colors1))
+//     expect(shuffleColors(colors2)).toEqual(shuffleColors(colors2))
+//   })
+// })
+
 // b) Create the function that makes the test pass.
 
+const shuffleColors = (arr) => {
+  arr.shift()
+  return arr.sort((a, b) => Math.random() - 0.5)
+}
+// console.log(shuffleColors(colors1))
+// console.log(shuffleColors(colors2))
+
 // Pseudo code:
+
+// Create a function that takes in a array and eliminates the first element and shuffles the rest
+// Within function use .shift() to remove frist element
+// Return array with sort method to shuffle the rest, using Math.random() 
+
 
 // --------------------2) Create a function that takes in an object that contains up votes and down votes and returns the end tally.
 
@@ -40,9 +60,27 @@ const votes1 = { upVotes: 13, downVotes: 2 }
 const votes2 = { upVotes: 2, downVotes: 33 }
 // Expected output: -31
 
+// describe("endTally", () => {
+//   it("returns an end tally of votes.", () => {
+//     expect(endTally(votes1)).toEqual(11)
+//     expect(endTally(votes2)).toEqual(-31)
+//   })
+// })
+
 // b) Create the function that makes the test pass.
 
+const endTally = (obj) => {
+  var totalTally = obj.upVotes - obj.downVotes
+  return totalTally
+}
+
+
 // Pseudo code:
+
+// create function thats take obj returns end tally of upVotes and downVotes
+// inside code block declare new variable totalTally
+// assign object upVotes - downVotes to new variable
+// return totalTally 
 
 // --------------------3) Create a function that takes in two arrays as arguments and returns one array with no duplicate values. STRETCH: Use the spread operator to pass in a dynamic number of arguments.
 
@@ -54,4 +92,25 @@ const dataTypesArray2 = ["string", "null", "Boolean", "string", "undefined"]
 
 // b) Create the function that makes the test pass.
 
+// const dataTypes = (arr1, arr2) => {
+//   mergedArr = [...arr1, ...arr2]
+//   var noDuplicates = [...new Set(mergedArr)]
+//   return noDuplicates
+// }
+// console.log(dataTypes(dataTypesArray1, dataTypesArray2))
+
+const dataTypes = (...data) => {
+  var noDuplicates = [...new Set(data)]
+  return noDuplicates 
+}
+
+mergedArr = [...dataTypesArray1, ...dataTypesArray2]
+console.log(dataTypes(...mergedArr))
+
+
 // Pseudo code:
+
+// Create function that accepts a dynamic number of arguments and returns an array with unique values
+// Declare variable and assign it the value [...new Set(data)]
+//the Set object accepts an array as arugment and returns a set with unique values. While the spread is used to hold the uniquw elements
+//we merge the the 2 arrays together using spread syntax and invoke dataTypes passing the mergedArr as a argument.
--- interview-questions.md
+++ interview-questions.md
@@ -8,42 +8,44 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What is object-oriented programming? How is it different than functional programming?
 
-Your answer:
+Your answer: OOP is a programming model that structures code by making objects and classes and functional focuses on writing functions and combining them to find solutions.While objects have their own traits and can execute actions 
 
-Researched answer:
+Researched answer: OOP is a programming paradigm that organizes code around objects, which encapsulate data and behavior. Functional programming treats coding as the evaluation of math functions
 
 2. What is the difference between a Float and an Integer in Ruby?
 
-Your answer:
+Your answer: A integer is a whole number and float is decimal number
 
-Researched answer:
+Researched answer: A Float is a floating-point number, which means it can represent both whole numbers and fractional values. An Integer is a whole number without any fractional or decimal part.
 
 3. Ruby has an implicit return. What does that mean?
 
-Your answer:
+Your answer: it means a method does not have return in it to return value. It is done automatically
 
-Researched answer:
+Researched answer: Implicit return automatically returning the value of the last evaluated expression in a method or block. 
 
 4. What is a block in Ruby?
 
-Your answer:
+Your answer: A block is chunk of code is that usually executed after a condtion is met or if a method invoked. 
 
-Researched answer:
+Researched answer: A block is a piece of code that can be passed to a method as an argument. 
 
 5. How do you extract a single value from a Ruby hash?
 
-Your answer:
+Your answer: to extract a value from a hash you must find the key that you trying to access.
 
-Researched answer:
+Researched answer: To access a single value from a hash, you must know the corresponding key is. Comparing a hash and a dictionary, where each key has a specific value associated with it.
+
+
 
 ## Looking Ahead: Terms for Next Week
 
-1. RSpec:
+1. RSpec: RSpec is a testing framework for the Ruby programming language 
 
-2. Test-driven development:
+2. Test-driven development: is a software development approach where tests are written before the actual code implementation.
 
-3. PostgreSQL:
+3. PostgreSQL: PostgreSQL is an open-source, object-relational database management system known for its reliability, and extensive feature set.
 
-4. CRUD:
+4. CRUD: is an acronym that stands for Create, Read, Update, and Delete. It refers to the four fundamental operations that can be performed on persistent data in a database or a software system.
 
-5. HTTP:
+5. HTTP: (Hypertext Transfer Protocol) is an application-layer protocol used for transmitting and receiving data over the internet.
